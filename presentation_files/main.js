4c532a934d40ce5515643e9d693c0458a0c2649f
Dmitry Dudin <dudin@xbsoftware.com>
2013-09-16 02:42:52 +0200

    web audio api update

--- presentation_files/main.js
+++ presentation_files/main.js
@@ -1,7 +1,16 @@
 $(function(){
+  window.timeUpdateInterval = false;
   $('.play').on('click', function(){
     window.playButtonClick();
     $('body').addClass('playing');
+    var playStartTime = new Date().getTime();
+    $('.pause').html('0 s');
+    if(timeUpdateInterval) clearInterval(timeUpdateInterval);
+    window.timeUpdateInterval = setInterval(function(){
+      var currentTime = new Date().getTime();
+      var val = Math.round((currentTime -playStartTime )/100)/10;
+      $('.pause').html( val+ (val==Math.ceil(val)?'.0':'') + ' s');
+    },100);
   });
   $('.pause').on('click', function(){
     window.stopButtonClick();
